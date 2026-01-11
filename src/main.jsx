b3f6147e0a850580ae6ae1c41cddc65bfcfa59c4
rajayush16 <ayush.raj12516@gmail.com>
2026-01-11 21:55:24 +0530

    Wire app providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+import { NotificationsProvider } from "./context/NotificationsContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <NotificationsProvider>
+        <App />
+      </NotificationsProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
