4229c3a344b2378f4ef7f837b893145fe1efed91
rajayush16 <ayush.raj12516@gmail.com>
2026-01-11 21:53:33 +0530

    Add API helper

---
+++ src/lib/api.js
@@ -0,0 +1,19 @@
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+
+export async function apiRequest(path, options = {}) {
+  const response = await fetch(`${API_BASE}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    credentials: "include",
+    ...options,
+  });
+
+  const data = await response.json().catch(() => ({}));
+  if (!response.ok) {
+    throw new Error(data.message || "Request failed");
+  }
+
+  return data;
+}
